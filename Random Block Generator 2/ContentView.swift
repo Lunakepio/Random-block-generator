8dbb8b92c1b9b6f0170a724b1a9941dcfdb03788
Lunakepio <moulinneufalex@gmail.com>
2024-05-16 08:25:53 +0200

    Initial Commit

---
+++ Random Block Generator 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Random Block Generator 2
+//
+//  Created by Alex Moulinneuf on 16/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Random Block Generator 2/Random_Block_Generator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Block_Generator_2App.swift
+//  Random Block Generator 2
+//
+//  Created by Alex Moulinneuf on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Random_Block_Generator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
